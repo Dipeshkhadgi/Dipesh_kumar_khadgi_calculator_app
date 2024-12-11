dbcab3c68c04f5edf5bdc47973ada822ef61b2a4
dipesh kumar khadgi <d8khadgi@gmail.com>
2024-12-11 13:04:53 +0545

    first calculator

---
+++ android/app/src/main/kotlin/com/example/calculatop_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.calculatop_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:calculatop_app/calculator_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: CalculatorView(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+//44 backgroundColor: const Color.fromARGB(255, 48, 241, 41),
+// 94 ? const Color.fromARGB(255, 231, 19, 19)
